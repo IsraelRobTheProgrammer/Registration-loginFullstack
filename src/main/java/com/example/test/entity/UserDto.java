d5c850a72cef3f99a34eefc09306f28e5b46d5ef
iamPius <dammyslim004@gmail.com>
2024-07-18 12:28:51 +0100

    connected to neon postgresql database

--- src/main/java/com/example/test/entity/UserDto.java
+++ src/main/java/com/example/test/entity/UserDto.java
@@ -27,26 +27,39 @@ public class UserDto {
     private String firstName;
     @Column(name = "Last Name", nullable = false)
     private String lastName;
+    @Column(name = "Matric Number", nullable = false, unique = true)
     private Long matric;
+    @Column(name = "Mode of Entry", nullable = false)
     private String modeOfEntry;
+    @Column(name = "Year of Entry", nullable = false)
     private int yearOfEntry;
+    @Column(name = "Email Address", nullable = false, unique = true)
     private String email;
+    @Column(name = "Password", nullable = false, unique = true)
     private String password;
+
     private String dob;
+    @Column(name = "Faculty", nullable = false)
     private String faculty;
+    @Column(name = "Department", nullable = false)
     private String department;
+    @Column(name = "Gender", nullable = false)
     private String gender;
+    @Column(name = "Academic_level", nullable = false)
     private int level;
+    @Column(nullable = false)
     private String stateOfOrigin;
+    @Column( nullable = false)
     private Long phoneNumber;
+    @Column( nullable = false)
     private int age;
-    @Column(name = "Parent/Guardian_Phone_Number")
+    @Column(name = "Parent/Guardian_Phone_Number", nullable = false)
     private Long parentPhoneNumber;
-    @Column(name = "Home/Residence_Address")
+    @Column(name = "Home/Residence_Address", nullable = false)
     private String homeAddress;
     @Column(name = "medical_Id", nullable = false)
     private String medicalId;
-    @Column (name="Local_Government")
+    @Column (name="Local_Government", nullable = false)
     private String localGovernment;
 
 
